51babaf553939b66f543742ab32b8772517274b5
Remy Blank <remy@c-space.org>
2024-08-09 18:53:20 +0200

    Ajout d'un makefile générique.

--- tdoc/common/__init__.py
+++ tdoc/common/__init__.py
@@ -2,8 +2,15 @@
 # Copyright 2024 Remy Blank <remy@c-space.org>
 # SPDX-License-Identifier: MIT
 
+import pathlib
+
 __version__ = '0.1'
+makefile = str(pathlib.Path(__file__).absolute().parent / 'common.mk')
 
 
 def setup(app):
-    return {'parallel_read_safe': True, 'parallel_write_safe': True}
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
