8857442dad689abd92e131a1ee16dd2ab6e2a858
Remy Blank <remy@c-space.org>
2024-09-07 21:31:58 +0200

    Prepare for 0.7 release.

--- tdoc/common/__init__.py
+++ tdoc/common/__init__.py
@@ -10,7 +10,7 @@ from sphinx.directives.code import CodeBlock
 from sphinx.util import logging
 
 __project__ = 't-doc-common'
-__version__ = '0.7.dev1'
+__version__ = '0.7'
 
 _common = pathlib.Path(__file__).absolute().parent
 _root = _common.parent.parent
