d588e6e603382da8b5faac0ce3f282d6b2a444a1
Remy Blank <remy@c-space.org>
2024-09-07 09:56:36 +0200

    Package all generated JavaScript as a single file.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -11,6 +11,9 @@ import license from 'rollup-plugin-license';
 const __filename = import.meta.filename || url.fileURLToPath(import.meta.url)
 const __dirname = import.meta.dirname || path.dirname(__filename)
 
+// TODO: Package @sqlite.org/sqlite-wasm. Or maybe it must be an external
+//       because of the worker module and .wasm.
+
 export default {
     input: "./tdoc/common/scripts/tdoc-editor.js",
     output: {
@@ -24,16 +27,7 @@ export default {
             constBindings: true,
             objectShorthand: true,
         },
-        // TODO: Package @sqlite.org/sqlite-wasm as well. Or maybe it must
-        //       be an external because of the worker module and .wasm
-        manualChunks: {
-            'tdoc-cm': ['codemirror'],
-            'tdoc-cm-css': ['@codemirror/lang-css'],
-            'tdoc-cm-html': ['@codemirror/lang-html'],
-            'tdoc-cm-javascript': ['@codemirror/lang-javascript'],
-            'tdoc-cm-python': ['@codemirror/lang-python'],
-            'tdoc-cm-sql': ['@codemirror/lang-sql'],
-        },
+        manualChunks: {},
     },
     plugins: [
         resolve(),
