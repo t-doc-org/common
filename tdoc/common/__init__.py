75fbc2710a5da6d6ad7aaba92db85221b700dbee
Remy Blank <remy@c-space.org>
2024-09-07 10:15:21 +0200

    Prepare for 0.6 release.

--- tdoc/common/__init__.py
+++ tdoc/common/__init__.py
@@ -10,7 +10,7 @@ from sphinx.directives.code import CodeBlock
 from sphinx.util import logging
 
 __project__ = 't-doc-common'
-__version__ = '0.6.dev3'
+__version__ = '0.6'
 
 _common = pathlib.Path(__file__).absolute().parent
 _root = _common.parent.parent
