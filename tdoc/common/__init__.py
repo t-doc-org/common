f5ed94ce92b93f90d32f01f4f4b865de0acd9572
Remy Blank <remy@c-space.org>
2024-09-12 22:01:57 +0200

    Build books using the latest stable release.

--- tdoc/common/__init__.py
+++ tdoc/common/__init__.py
@@ -10,7 +10,7 @@ from sphinx.directives.code import CodeBlock
 from sphinx.util import logging
 
 __project__ = 't-doc-common'
-__version__ = '0.9'
+__version__ = '0.10.dev1'
 
 _common = pathlib.Path(__file__).absolute().parent
 _root = _common.parent.parent
