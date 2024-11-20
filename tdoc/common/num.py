9fd2feeb53e5a0d76176696fbe14232d05a04ce1
Remy Blank <remy@c-space.org>
2024-11-20 12:01:36 +0100

    Allow disabling numbering selectively per enumerable node type.

--- tdoc/common/num.py
+++ tdoc/common/num.py
@@ -4,32 +4,47 @@
 import pathlib
 
 from docutils import nodes
+from sphinx import errors
 from sphinx.util import docutils, logging
 
 from . import __version__
 
 _log = logging.getLogger(__name__)
 
-# TODO: Selectively disable entries in numfig_format by setting the dict values
-#       to an object implementing __mod__() and returning an empty string
-# TODO: Substitute None in numfig_format with NoNum
-
-class NoNum:
-    # TODO: Differentiate between rendering for {numref} or for a caption?
-    #       Doesn't make sense to have {numref} if the number is never displayed
-    def __contains__(self, value): return False
-    def __mod__(self, other): return ''
-
 
 def setup(app):
     app.add_role('num', Num())
-    app.add_enumerable_node(num, 'num', get_num_title, html=(visit_num, depart_num))
+    app.add_enumerable_node(num, 'num', lambda n: True,
+                            html=(visit_num, depart_num))
+    app.connect('config-inited', update_numfig_format)
     app.connect('doctree-resolved', update_num_nodes)
     return {
         'version': __version__,
         'parallel_read_safe': True,
         'parallel_write_safe': True,
     }
+
+
+class NoNum:
+    def __contains__(self, value): return False
+    def __mod__(self, other): return ''
+
+
+def update_numfig_format(app, config):
+    # Disable numbering by default for all standard enumerable node types except
+    # 'section', and set the default format for 'num'.
+    numfig_format = config.numfig_format
+    for k in ('code-block', 'figure', 'section', 'table'):
+        if numfig_format.setdefault(k, k == 'section') is True:
+            del numfig_format[k]
+    numfig_format.setdefault('num', '%s')
+    print(numfig_format)
+    for k, v in numfig_format.items():
+        if v is False:
+            if k == 'section':
+                raise errors.ConfigError(
+                    f"numfig_format: Numbering cannot be disabled for '{k}'")
+            numfig_format[k] = NoNum()
 
 
 class Num(docutils.ReferenceRole):
@@ -44,35 +59,33 @@ class Num(docutils.ReferenceRole):
 class num(nodes.Inline, nodes.TextElement): pass
 
 
-def get_num_title(node):
-    return True  # The actual value doesn't matter, as long as it's truthy
-
-
 def update_num_nodes(app, doctree, docname):
-    add_num_content(app.env, doctree, docname)
+    for node, text in find_num(app.env, doctree, docname):
+        del node['title']
+        node += [text]
+
     # TOCs are extracted on doctree-read, as a transform with priority=880,
     # while toc_fignumbers are only assigned later, as a post-transform, shortly
     # before doctree-resolved. So the num nodes in the TOCs must be updated
-    # separately.
-    add_num_content(app.env, app.env.tocs[docname], docname)
+    # separately. Replace the node by its text altogether, to avoid duplicate
+    # 'id' attributes.
+    for node, text in find_num(app.env, app.env.tocs[docname], docname):
+        node.parent.replace(node, text)
 
 
-def add_num_content(env, doctree, docname):
-    fignumbers = env.toc_fignumbers.get(docname, {})
+def find_num(env, doctree, docname):
+    fignumbers = env.toc_fignumbers.get(docname, {}).get('num')
     for node in doctree.findall(num):
-        numbers = fignumbers['num'][node['ids'][0]]
+        if fignumbers is None:
+            raise errors.ConfigError(":num: node found, but numfig is disabled")
+        numbers = fignumbers[node['ids'][0]]
         num_str = '.'.join(map(str, numbers))
-        node += [nodes.Text(node['title'] % num_str)]
+        yield node, nodes.Text(node['title'] % num_str)
 
 
 def visit_num(self, node):
-    # When the role is used in a heading, it can be rendered multiple times,
-    # e.g. for the document and for the TOC. Only the document render must
-    # include the ID.
-    render_doc = pathlib.Path(node.document['source']).exists()
-    if render_doc: self.body.append(self.starttag(node, 'span', suffix=''))
+    self.body.append(self.starttag(node, 'span', suffix=''))
 
 
 def depart_num(self, node):
-    render_doc = pathlib.Path(node.document['source']).exists()
-    if render_doc: self.body.append('</span>')
+    self.body.append('</span>')
