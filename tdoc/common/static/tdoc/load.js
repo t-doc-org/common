db20b0d68eb2acdd5fd112164100ce4c3c102bfb
Remy Blank <remy@c-space.org>
2025-04-13 20:19:37 +0200

    Make the drawing toolbar more compact.

--- tdoc/common/static/tdoc/load.js
+++ tdoc/common/static/tdoc/load.js
@@ -27,6 +27,10 @@ domLoaded.then(() => {
 
 // Handle the "draw" button.
 let drawing, drawingSvg;
+const drawState = {
+    tool: 'stylus', eraser: false,
+    size: 3, color: '#ff0000', marker: false,
+};
 globalThis.tdocDraw = () => {
     const ds = document.documentElement.dataset;
     if (ds.tdocDraw !== undefined) {
@@ -40,47 +44,49 @@ globalThis.tdocDraw = () => {
         return;
     }
 
-    const markerFact = 8;
-    function brushSize() {
-        return drawing.brush.size
-            / (drawing.brush.color.slice(7) !== 'ff' ? markerFact : 1)
-    }
-    function setBrush(opts) {
-        const col = opts.color ?? drawing.brush.color.slice(0, 7);
-        const tr = opts.transparent ?? (drawing.brush.color.slice(7) !== 'ff');
-        const size = opts.size ?? brushSize();
-        drawing.brush = {
-            ...drawing.brush,
-            color: col + (tr ? '40' : 'ff'),
-            size: size * (tr ? markerFact : 1),
-        };
+    function setState(opts) {
+        Object.assign(drawState, opts);
+        const mode = drawState.eraser ? 'eraseLine' :
+                     drawState.tool === 'arrow' ? 'line' : drawState.tool;
+        if (drawing.mode !== mode) drawing.mode = mode;
+        drawing.brush.arrowEnd = drawState.tool === 'arrow';
+        drawing.brush.size = drawState.size * (drawState.marker ? 8 : 1);
+        drawing.brush.color = drawState.color
+                              + (drawState.marker ? '40' : 'ff');
     }
 
     drawingSvg = qs(document, '.bd-content').appendChild(elmt`\
 <svg id="tdoc-drawing" xmlns="http://www.w3.org/2000/svg"\
  xmlns:xlink="http://www.w3.org/1999/xlink"></svg>`);
-    drawing = createDrauu({
-        el: drawingSvg,
-        brush: {mode: 'stylus', color: `#ff0000ff`, size: 3},
-    });
+    drawing = createDrauu({el: drawingSvg});
+    setState({});
     // TODO: Don't capture 'keydown' events on window
     // TODO: Use bootstrap tooltips instead of title=
     const toolbar = qs(document, '.header-article-items__start')
         .appendChild(elmt`\
 <div class="header-article-item tdoc-draw-toolbar">\
-<input type="radio" name="tool" class="btn fa-pen" data-mode="stylus"\
- title="Stylus">\
-<input type="radio" name="tool" class="btn fa-pencil" data-mode="draw"\
- title="Pencil">\
-<input type="radio" name="tool" class="btn fa-pen-ruler" data-mode="line"\
- title="Line">\
-<input type="radio" name="tool" class="btn fa-arrow-right"\
- data-mode="line" data-arrow="y" title="Arrow">\
-<input type="radio" name="tool" class="btn fa-eraser"\
+<div class="tdoc-tool dropdown-center">\
+<button class="btn" title="Tool" data-bs-toggle="dropdown"></button>\
+<ul class="dropdown-menu">\
+<li><a class="dropdown-item fa-paintbrush" data-tool="stylus"\
+ title="Brush"></a></li>\
+<li><a class="dropdown-item fa-pencil" data-tool="draw"\
+ title="Pencil"></a></li>\
+<li><a class="dropdown-item fa-pen-ruler" data-tool="line"\
+ title="Line"></a></li>\
+<li><a class="dropdown-item fa-arrow-right" data-tool="arrow"\
+ title="Arrow"></a></li>\
+<li><a class="dropdown-item fa-square" data-tool="rectangle"\
+ title="Rectangle"></a></li>\
+<li><a class="dropdown-item fa-circle" data-tool="ellipse"\
+ title="Ellipse"></a></li>\
+</ul>\
+</div>\
+<input type="checkbox" name="eraser" class="btn fa-eraser"\
  data-mode="eraseLine" title="Eraser">\
 <button name="clear" class="btn fa-trash" title="Clear"></button>\
 <input class="tdoc-size" type="range" min="1" max="8" step="1"\
- value="${brushSize()}" title="Size">\
+ value="${drawState.size}" title="Stroke width">\
 <div class="tdoc-color dropdown-center">\
 <button class="btn fa-square" title="Color" data-bs-toggle="dropdown"></button>\
 <ul class="dropdown-menu">\
@@ -94,36 +100,36 @@ globalThis.tdocDraw = () => {
 </div>\
 <input type="checkbox" name="marker" class="btn fa-marker" title="Marker">\
 </div>`);
-    const toolBtns = qsa(toolbar, '[name=tool]');
-    for (const el of toolBtns) {
-        if (el.dataset.mode === drawing.mode
-                && !!el.dataset.arrow === !!drawing.brush.arrowEnd) {
-            el.checked = true;
-        }
-        el.addEventListener('click', e => {
-            drawing.mode = e.target.dataset.mode;
-            drawing.brush.arrowEnd = !!e.target.dataset.arrow;
+    const toolButton = qs(toolbar, '.tdoc-tool button');
+    for (const el of qsa(toolbar, '.tdoc-tool .dropdown-item')) {
+        const tool = el.dataset.tool, icon = el.classList[1];
+        if (drawState.tool === tool) toolButton.classList.add(icon);
+        el.addEventListener('click', () => {
+            toolButton.classList.remove(toolButton.classList[1]);
+            toolButton.classList.add(icon);
+            setState({tool});
         });
     }
+    qs(toolbar, '[name=eraser]').addEventListener('click', e => {
+        setState({eraser: e.target.checked});
+    });
     qs(toolbar, '[name=clear]').addEventListener('click', () => {
         drawing.clear();
     });
     qs(toolbar, '.tdoc-size').addEventListener('input', e => {
-        setBrush({size: +e.target.value});
+        setState({size: +e.target.value});
     });
     const colorButton = qs(toolbar, '.tdoc-color button');
     for (const el of qsa(toolbar, '.tdoc-color .dropdown-item')) {
         const color = rgb2hex(el.style.color);
-        if (color == drawing.brush.color.slice(0, 7)) {
-            colorButton.style.color = color;
-        }
+        if (drawState.color === color) colorButton.style.color = color;
         el.addEventListener('click', () => {
             colorButton.style.color = color;
-            setBrush({color});
+            setState({color});
         });
     }
     qs(toolbar, '[name=marker]').addEventListener('click', e => {
-        setBrush({transparent: e.target.checked});
+        setState({marker: e.target.checked});
     });
 };
 
