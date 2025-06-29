a6bfcc0dae8547638020f877223a3571b5dde5c3
Remy Blank <remy@c-space.org>
2025-06-29 23:02:22 +0200

    Unbundle Pyodide and SQLite.

--- tools/hatch_hooks.py
+++ tools/hatch_hooks.py
@@ -18,12 +18,6 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from hatchling.metadata.plugin.interface import MetadataHookInterface
 
 LICENSES = 'LICENSES.deps.txt'
-pyodide_rel = 'https://github.com/t-doc-org/pyodide-packages/releases/download'
-pyodide_packages = [
-    r'micropip-\d+(?:\.\d+)+.*\.whl',
-    r'packaging-\d+(?:\.\d+)+.*\.whl',
-    r'sqlite3-\d+(?:\.\d+)+.*\.(?:whl|zip)',
-]
 
 
 class HookMixin:
@@ -108,38 +102,8 @@ class BuildHook(BuildHookInterface, HookMixin):
         self.copytree_node('polyscript/dist', 'polyscript', globs=[
             '*.js', '*.js.map',
         ])
-        self.copytree_node('pyodide', 'pyodide', globs=[
-            'pyodide.asm.*', 'pyodide-lock.json', 'pyodide.mjs',
-            'pyodide.mjs.map', 'python_stdlib.zip',
-        ])
         self.copy_node('sabayon/dist/sw-listeners.js', 'sabayon-listeners.js')
-        self.copytree_node(
-            '@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm', 'sqlite', globs=[
-                'sqlite3-bundler-friendly.mjs',
-                'sqlite3-opfs-async-proxy.js',
-                'sqlite3-worker1-bundler-friendly.mjs',
-                'sqlite3-worker1-promiser.mjs',
-                'sqlite3.wasm',
-            ])
         self.run([npm, 'run', 'build'])
-
-        self.app.display_info("Fetching Pyodide packages")
-        for name, pkg in self.package_lock['packages'].items():
-            if name == 'node_modules/pyodide':
-                v = pkg['version']
-                break
-        else:
-            raise Exception('pyodide not found in package-lock.json')
-        pat = re.compile('|'.join(pyodide_packages))
-        with HttpFile(f'{pyodide_rel}/{v}/pyodide-{v}.zip') as hf, \
-                zipfile.ZipFile(hf, mode='r') as zf:
-            for zi in zf.infolist():
-                if zi.is_dir(): continue
-                name = pathlib.Path(zi.filename).name
-                if pat.fullmatch(name) is None: continue
-                with zf.open(zi) as f:
-                    dst = pathlib.Path(self.static_gen / 'pyodide' / name)
-                    dst.write_bytes(f.read())
 
     def copy_node(self, src, dst):
         shutil.copy2(self.node_modules / src, self.static_gen / dst)
@@ -157,74 +121,3 @@ class BuildHook(BuildHookInterface, HookMixin):
                              stderr=subprocess.STDOUT, text=True)
         if res.returncode != 0:
             self.app.abort(f"Command failed:\n{res.stdout}")
-
-
-class HttpFile(io.RawIOBase):
-    def __init__(self, url):
-        self._offset = 0
-        while True:
-            parts = parse.urlsplit(url, allow_fragments=False)
-            self._req = parse.urlunsplit(('', '') + parts[2:])
-            if parts.scheme == 'http':
-                cls = http.client.HTTPConnection
-            elif parts.scheme == 'https':
-                cls = http.client.HTTPSConnection
-            else:
-                raise ValueError(f"Unsupported URL: {url}")
-            conn = cls(parts.netloc)
-            try:
-                conn.request('HEAD', self._req)
-                resp = conn.getresponse()
-                resp.read()
-                if resp.status == http.HTTPStatus.OK:
-                    if resp.headers['Accept-Ranges'] != 'bytes':
-                        raise Exception("Range requests unsupported")
-                    self._size = int(resp.headers['Content-Length'])
-                    self._conn, conn = conn, None
-                    break
-                elif resp.status in (http.HTTPStatus.MOVED_PERMANENTLY,
-                                     http.HTTPStatus.FOUND,
-                                     http.HTTPStatus.TEMPORARY_REDIRECT,
-                                     http.HTTPStatus.PERMANENT_REDIRECT):
-                    url = resp.headers['Location']
-                    continue
-                raise Exception(f"Request failed: {resp.status} {resp.reason}")
-            finally:
-                if conn is not None: conn.close()
-
-    def close(self):
-        if hasattr(self, '_conn'): self._conn.close()
-
-    def __enter__(self): return self
-    def __exit__(self, typ, value, tb): self.close()
-
-    def read(self, size=-1, /):
-        if size == 0: return b''
-        elif size < 0: size = self._size - self._offset
-        elif self._offset + size > self._size: size = self._size - self._offset
-        self._conn.request('GET', self._req, headers={
-            'Range': f'bytes={self._offset}-{self._offset + size - 1}',
-        })
-        resp = self._conn.getresponse()
-        data = resp.read()
-        if resp.status != http.HTTPStatus.PARTIAL_CONTENT:
-            raise Exception(f"Request failed: {resp.status} {resp.reason}")
-        self._offset += len(data)
-        return data
-
-    def seek(self, offset, whence=os.SEEK_SET, /):
-        off = self._offset
-        if whence == os.SEEK_SET:
-            off = offset
-        elif whence == os.SEEK_CUR:
-            off += offset
-        elif whence == os.SEEK_END:
-            off = self._size + offset
-        else:
-            raise ValueError(f"Invalid whence argument: {whence}")
-        self._offset = 0 if off < 0 else self._size if off > self._size else off
-        return self._offset
-
-    def readable(self): return True
-    def seekable(self): return True
-    def tell(self): return self._offset
