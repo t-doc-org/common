3e0eba843c9417ec0530fe152ecd755981a3769a
Remy Blank <remy@c-space.org>
2025-07-10 16:41:05 +0200

    Update polyscript and pyodide.

--- tdoc/common/deps.py
+++ tdoc/common/deps.py
@@ -5,22 +5,32 @@ jsdelivr = 'https://cdn.jsdelivr.net'
 
 info = {
     'drauu': {
+        # https://www.npmjs.com/package/@drauu/core?activeTab=versions
+        # https://github.com/antfu/drauu/tags
         'version': '0.4.3',
         'url': lambda v: f'{jsdelivr}/npm/@drauu/core@{v}/dist',
     },
     'mathjax': {
+        # https://www.npmjs.com/package/mathjax?activeTab=versions
+        # https://github.com/mathjax/MathJax/releases
         'version': '3.2.2',
         'url': lambda v: f'{jsdelivr}/npm/mathjax@{v}/es5',
     },
     'polyscript': {
-        'version': '0.17.30',
+        # https://www.npmjs.com/package/polyscript?activeTab=versions
+        # https://github.com/pyscript/polyscript/tags
+        'version': '0.18.2',
         'url': lambda v: f'{jsdelivr}/npm/polyscript@{v}/dist',
     },
     'pyodide': {
-        'version': '0.27.7',
+        # https://www.npmjs.com/package/pyodide?activeTab=versions
+        # https://pyodide.org/en/stable/project/changelog.html
+        'version': '0.28.0',
         'url': lambda v: f'{jsdelivr}/pyodide/v{v}/full',
     },
     'sqlite': {
+        # https://www.npmjs.com/package/@sqlite.org/sqlite-wasm?activeTab=versions
+        # https://github.com/sqlite/sqlite-wasm/releases
         'version': '3.50.1-build1',
         'url': lambda v: f'{jsdelivr}/npm/@sqlite.org/sqlite-wasm@{v}',
     },
