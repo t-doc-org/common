b0e343d8c21d6d5034c45c49f865d9d296e04e9c
Remy Blank <remy@c-space.org>
2025-09-27 17:55:25 +0200

    Add a script to generate requirements files.

---
+++ tools/requirements.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright 2025 Remy Blank <remy@c-space.org>
+# SPDX-License-Identifier: MIT
+
+set -o errexit -o pipefail -o nounset
+shopt -s nullglob
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $(basename "$0") SPEC"
+    exit 2
+fi
+
+uv export --no-header --format=requirements-txt --script=<(
+cat <<EOF
+# /// script
+# requires-python = '>=3.12'
+# dependencies = ['$1']
+# ///
+EOF
+)
