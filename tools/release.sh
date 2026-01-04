d60f31fdc10c58e56d0b35bde0683ad16f70d4a7
Remy Blank <remy@c-space.org>
2026-01-04 19:14:11 +0100

    Add post-release instructions.

--- tools/release.sh
+++ tools/release.sh
@@ -26,3 +26,18 @@ fi
 
 hg -R "${common}" ci -m "Release ${version}."
 hg -R "${common}" tag -m "Tag ${version} release." "${version}"
+
+cat <<EOF
+
+After the release has been pushed and built successfully:
+ - Increment __version__ in tdoc/common/__init__.py and add a ".dev1" suffix.
+ - Generate the requirements file for the release, add the new file, commit and
+   push.
+   $ tools/release-reqs.sh ${version}
+   $ hg add config/${version}.req
+   $ hg commit -m "Add requirements for ${version}." \\
+       tdoc/common/__init__.py config/${version}.req
+ - In config/t-doc.toml, move the current value of "stable" to "previous", set
+   "stable" to ${version}, commit and push.
+   $ hg commit -m "Roll out ${version} to stable." config/t-doc.toml
+EOF
