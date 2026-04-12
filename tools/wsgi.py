50ebdbef7b65d7f92bcb6756fdd070a30de29a73
Remy Blank <remy@c-space.org>
2026-04-12 19:08:13 +0200

    Add a wsgi script for running the API server.

---
+++ tools/wsgi.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Remy Blank <remy@c-space.org>
+# SPDX-License-Identifier: MIT
+
+import mod_wsgi
+import pathlib
+import sys
+
+from tdoc.common import api_mod_wsgi, logs, wsgi
+
+base = pathlib.Path(__file__).parent.parent.resolve()
+sys.stderr.write(
+    f"[{base.name}] Starting (processes: {mod_wsgi.maximum_processes}, "
+    f"threads: {mod_wsgi.threads_per_process})\n")
+
+application = api_mod_wsgi.application(
+    config_path=base / '_local' / 'config.toml',
+    origins=rf'https://(?:{wsgi.hostname_re}\.)?t-doc\.org',
+    events_level=logs.NOTSET)
