4d53c8a8ece8553221bcd120ac1f149499257c6b
Remy Blank <remy@c-space.org>
2026-08-11 11:48:33 +0200

    Roll back jsxgraph to 1.12.2.

--- tdoc/common/deps.py
+++ tdoc/common/deps.py
@@ -104,7 +104,7 @@ info = {
         # BUG(jsxgraph-1.13.1): SVG rendering may break for functions with
         # large values (e.g. tan(x), 1/x)
         # <https://github.com/jsxgraph/jsxgraph/issues/824>.
-        'version': '1.13.1',
+        'version': '1.12.2',
         'tag': 'latest',
         'version_tag': lambda v: f'v{v}',
         'cdn': lambda n, v: f'{jsdelivr}/npm/{n}@{v}/distrib',
